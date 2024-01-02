4c5fd8492377dc4d4fef8d568aec930282bda3c6
artshil <iashilraj@gmail.com>
2024-01-02 21:59:49 +0530

    logo link

--- src/Components/Header/HeaderComponent.jsx
+++ src/Components/Header/HeaderComponent.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 //import './Header.css';
 
 const Header = () => {
+  const handleReload = () => {
+    window.location.reload();
+  };  
   return (
     <header className="header">
-      <a href='/'><img src="https://i.imgur.com/IoGsmhr.png"></img></a>
+      <a href="#" onClick={handleReload}><img src="https://i.imgur.com/IoGsmhr.png"></img></a>
       
     </header>
   );
